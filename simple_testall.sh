a78882ccd331ff862ddc4b9d510a9940e2830350
snpark <snpark@student.42seoul.kr>
2022-06-24 17:52:49 +0900

    no enter

---
+++ simple_testall.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+trap "pkill -P $$" SIGINT
+trap "pkill -P $$" SIGTERM
+trap "pkill -P $$" SIGQUIT
+
+make re -C ../
+make re -C ./resources/terminator/
+
+../ircserv 6667 6667 > /dev/null 2> /dev/null&
+clear
+
+TEST=$(/bin/ls ./testcases/case)
+
+for test in ${TEST[@]}
+do
+	clear
+	./test.sh $test
+done
+
+make fclean -C ../
+make fclean -C ./resources/terminator/
+pkill -P $$
