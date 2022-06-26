7373f35c99d5c39d61a32a338e17e5e9f5d332cb
snpark <snpark@student.42seoul.kr>
2022-06-26 11:11:09 +0900

    main함수 쪼개기 완료

--- resources/terminator/srcs/main.cpp
+++ resources/terminator/srcs/main.cpp
@@ -9,94 +9,74 @@
 #include <sys/stat.h>
 #include <vector>
 
-int main(int argc, char **argv)
+std::vector<Client *> clients;
+std::string test_case_name;
+std::ifstream test_case;
+std::fstream reference;
+std::string message;
+std::string buffer;
+Client *client;
+Event event;
+int result;
+int count;
+int port;
+int target;
+bool skip = false;
+
+void compare_result()
 {
-    std::ifstream test_case;
-    std::string test_case_name;
-    std::vector<Client *> clients;
-    Event event;
-    int target;
-    Client *client;
-    std::string message;
-    int result;
-    bool skip = false;
-
-    if (argc < 3)
+    // 로그 읽어와서 비교하기
+    std::string test;
+    for (int i = 0, size = clients.size(); i < size; ++i)
+        test += "\r\n" + clients[i]->nick + "\r\n" + clients[i]->buffer;
+    buffer.resize(test.size());
+    reference.open("./testcases/reference/" + test_case_name, std::fstream::in);
+    if (reference.is_open())
     {
-        std::cerr << argv[0] << " <filename>" << std::endl;
-        return 1;
-    }
-    test_case_name = argv[1];
-    test_case.open(test_case_name);
-    if (test_case.fail())
-    {
-        std::cerr << argv[1] << " is not open" << std::endl;
-        return 1;
-    }
-    test_case_name.erase(0, test_case_name.find_first_of('/') + 1);
-    test_case_name.erase(0, test_case_name.find_first_of('/') + 1);
-    test_case_name.erase(0, test_case_name.find_first_of('/') + 1);
-    std::string buffer;
-    //연결할 서버 port
-    std::getline(test_case, buffer);
-    int port = atoi(buffer.c_str());
-    // kqueue 생성
-    event.initialize();
-
-    // 클라이언트를 서버에 연결
-    std::getline(test_case, buffer);
-    std::istringstream iss(buffer);
-    struct kevent tmp;
-    while (std::getline(iss, buffer, ','))
-    {
-        if (buffer.back() == '!')
+        reference.read(buffer.begin().base(), test.size());
+        std::cout << "===== reference ======" << buffer;
+        if (buffer == test)
+            std::cout << '\n'
+                      << test_case_name << ":\033[1;32m ok\033[0m" << std::endl;
+        else
         {
-            skip = true;
-            buffer.pop_back();
+            std::cout << "===== test ======" << test;
+            std::cout << '\n'
+                      << test_case_name << ":\033[1;31m ko\033[0m" << std::endl;
+            std::fstream test_file;
+            mkdir("./diff", 0755);
+            std::string test_dir = "./diff/" + test_case_name.substr(0, test_case_name.find_first_of('/'));
+            mkdir(test_dir.data(), 0755);
+            test_file.open("./diff/" + test_case_name, std::fstream::out | std::fstream::trunc);
+            if (test_file.is_open())
+            {
+                test_file << test;
+                test_file.flush();
+            }
+            std::string diff_str = "diff ./testcases/reference/"
+				+ test_case_name + " ./diff/" + test_case_name +
+				" > ./diff/" + test_case_name + ".diff || cat ./diff/"
+				+ test_case_name + ".diff";
+            std::cout << diff_str << std::endl;
+            system(diff_str.c_str());
         }
-        clients.push_back(new Client(buffer, argv[2], port));
-        event.m_set(clients.back()->fd, EVFILT_WRITE, EV_ADD, 0, 0,
-                    clients.back());
-        event.m_set(clients.back()->fd, EVFILT_READ, EV_ADD, 0, 0,
-                    clients.back());
-        if (!skip)
-        {
-            buffer = "pass 6667\r\nnick " + clients.back()->nick + "\r\nuser testuser testhost testserver :realname\r\n";
-            while (kevent(event._kqueue, NULL, 0, &tmp, 1, &event.timer))
-                if ((int)tmp.ident == clients.back()->fd)
-                {
-                    if (tmp.filter == EVFILT_WRITE)
-                    {
-                        result = write(clients.back()->fd, buffer.data(),
-                                       buffer.size());
-                        if (result)
-                            buffer.erase(0, result);
-                    }
-                    if (tmp.filter == EVFILT_READ)
-                    {
-                        buffer.resize(tmp.data);
-                        if (read(clients.back()->fd, (void *)buffer.data(),
-                                 tmp.data))
-                            clients.back()->buffer.append(buffer);
-                        break;
-                    }
-                }
-            std::cout << std::setiosflags(std::ios_base::left) << std::setw(9)
-                      << clients.back()->nick << ": [PASS,NICK,USER]\n";
-        }
-        event.m_set(clients.back()->fd, EVFILT_WRITE, EV_DISABLE, 0, 0,
-                    clients.back());
-        skip = false;
     }
-    if (std::getline(test_case, message, ' '))
-    {
-        target = atoi(message.data());
-        std::getline(test_case, message);
-        event.m_set(clients[target]->fd, EVFILT_WRITE, EV_ENABLE, 0, 0,
-                    clients[target]);
-        message += "\r\n";
-    }
-    int count;
+}
+
+void make_reference()
+{
+	reference.open("./testcases/reference/" + test_case_name,
+			std::fstream::out | std::fstream::trunc);
+	if (reference.is_open())
+	{
+		for (int i = 0, size = clients.size(); i < size; ++i)
+			reference << "\r\n" << clients[i]->nick << "\r\n" << clients[i]->buffer;
+		reference.close();
+	}
+}
+
+int process_testcase()
+{
     while ((count = event.kevent()) > 0)
     {
         for (int i = 0; i < count; ++i)
@@ -144,49 +124,105 @@ int main(int argc, char **argv)
             }
         }
     }
-    std::fstream std;
-        //std.open("./testcases/reference/" + test_case_name, std::fstream::out | std::fstream::trunc);
-        //if (std.is_open())
-        //{
-        //    for (int i = 0, size = clients.size(); i < size; ++i)
-        //        std << "\r\n" << clients[i]->nick << "\r\n" << clients[i]->buffer;
-        //    std.close();
-        //}
+	return 0;
+}
 
-    // 로그 읽어와서 비교하기
-    std::string test;
-    for (int i = 0, size = clients.size(); i < size; ++i)
-        test += "\r\n" + clients[i]->nick + "\r\n" + clients[i]->buffer;
-    buffer.resize(test.size());
-    std.open("./testcases/reference/" + test_case_name, std::fstream::in);
-    if (std.is_open())
+void prepare_first_message()
+{
+    if (std::getline(test_case, message, ' '))
     {
-        std.read(buffer.begin().base(), test.size());
-        std::cout << "===== reference ======" << buffer;
-        if (buffer == test)
-            std::cout << '\n'
-                      << test_case_name << ":\033[1;32m ok\033[0m" << std::endl;
-        else
-        {
-            std::cout << "===== test ======" << test;
-            std::cout << '\n'
-                      << test_case_name << ":\033[1;31m ko\033[0m" << std::endl;
-            std::fstream test_file;
-            mkdir("./diff", 0755);
-            std::string test_dir = "./diff/" + test_case_name.substr(0, test_case_name.find_first_of('/'));
-            mkdir(test_dir.data(), 0755);
-            test_file.open("./diff/" + test_case_name, std::fstream::out | std::fstream::trunc);
-            if (test_file.is_open())
-            {
-                test_file << test;
-                test_file.flush();
-            }
-            std::string diff_str = "diff ./testcases/reference/"
-				+ test_case_name + " ./diff/" + test_case_name +
-				" > ./diff/" + test_case_name + ".diff || cat ./diff/"
-				+ test_case_name + ".diff";
-            std::cout << diff_str << std::endl;
-            system(diff_str.c_str());
-        }
+        target = atoi(message.data());
+        std::getline(test_case, message);
+        event.m_set(clients[target]->fd, EVFILT_WRITE, EV_ENABLE, 0, 0,
+                    clients[target]);
+        message += "\r\n";
     }
+}
+
+void connect_client_to_server(char *ip)
+{
+    // 클라이언트를 서버에 연결
+    std::getline(test_case, buffer);
+    std::istringstream iss(buffer);
+    struct kevent tmp;
+    while (std::getline(iss, buffer, ','))
+    {
+        if (buffer.back() == '!')
+        {
+            skip = true;
+            buffer.pop_back();
+        }
+        clients.push_back(new Client(buffer, ip, port));
+        event.m_set(clients.back()->fd, EVFILT_WRITE, EV_ADD, 0, 0,
+                    clients.back());
+        event.m_set(clients.back()->fd, EVFILT_READ, EV_ADD, 0, 0,
+                    clients.back());
+        if (!skip)
+        {
+            buffer = "pass 6667\r\nnick " + clients.back()->nick +
+				"\r\nuser testuser testhost testserver :realname\r\n";
+            while (kevent(event._kqueue, NULL, 0, &tmp, 1, &event.timer))
+                if ((int)tmp.ident == clients.back()->fd)
+                {
+                    if (tmp.filter == EVFILT_WRITE)
+                    {
+                        result = write(clients.back()->fd, buffer.data(),
+                                       buffer.size());
+                        if (result)
+                            buffer.erase(0, result);
+                    }
+                    if (tmp.filter == EVFILT_READ)
+                    {
+                        buffer.resize(tmp.data);
+                        if (read(clients.back()->fd, (void *)buffer.data(),
+                                 tmp.data))
+                            clients.back()->buffer.append(buffer);
+                        break;
+                    }
+                }
+            std::cout << std::setiosflags(std::ios_base::left) << std::setw(9)
+                      << clients.back()->nick << ": [PASS,NICK,USER]\n";
+        }
+        event.m_set(clients.back()->fd, EVFILT_WRITE, EV_DISABLE, 0, 0,
+                    clients.back());
+        skip = false;
+    }
+}
+
+
+int initialize(int argc, char **argv)
+{
+    if (argc < 3)
+    {
+        std::cerr << argv[0] << " <filename> <ip>" << std::endl;
+        return 1;
+    }
+    test_case_name = argv[1];
+    test_case.open(test_case_name);
+    if (test_case.fail())
+    {
+        std::cerr << argv[1] << " is not open" << std::endl;
+        return 1;
+    }
+    test_case_name.erase(0, test_case_name.find_first_of('/') + 1);
+    test_case_name.erase(0, test_case_name.find_first_of('/') + 1);
+    test_case_name.erase(0, test_case_name.find_first_of('/') + 1);
+    //연결할 서버 port
+    std::getline(test_case, buffer);
+    port = atoi(buffer.c_str());
+    // kqueue 생성
+    event.initialize();
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	if (initialize(argc, argv))
+		return 1;
+	connect_client_to_server(argv[2]);
+	prepare_first_message();
+	if (process_testcase())
+		return 1;
+	//make_reference();
+	compare_result();
 }
