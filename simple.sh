a262cf9f7f6676da295dccd47db6bc80f9afa441
snpark <snpark@student.42seoul.kr>
2022-06-26 16:51:53 +0900

    simple 명령어만 집어넣으면 되게 변경

--- simple.sh
+++ simple.sh
@@ -4,25 +4,27 @@ trap "pkill -P $$; exit" SIGINT
 trap "pkill -P $$; exit" SIGTERM
 trap "pkill -P $$; exit" SIGQUIT
 
-if [ $# -eq 2 ]; then
-	IP=$1
+if [ $# -eq 3 ]; then
+	IP=$2
 else
 	IP="127.0.0.1"
 fi
 
+name=$(ls ./testcases/case | grep $1)
+
 make re -C ../
 make re -C ./resources/terminator/
-rm -rf diff/$1
+rm -rf diff/$name
 
 ../ircserv 6667 6667 > /dev/null 2> /dev/null&
 clear
 
-CASE=$(/bin/ls ./testcases/case/$1)
+CASE=$(/bin/ls ./testcases/case/$name)
 for case in ${CASE[@]}
 do
-	echo $1
+	echo $name
 	echo $case
-	./resources/terminator/tester ./testcases/case/$1/$case $IP
+	./resources/terminator/tester ./testcases/case/$name/$case $IP
 done
 
 make fclean -C ../
