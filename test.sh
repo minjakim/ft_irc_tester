ca8feb4e23da2ea731b754afafc6f45ca5849ee8
snpark <snpark@student.42seoul.kr>
2022-06-26 12:47:51 +0900

    쉽게 실행

--- test.sh
+++ test.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
+trap "pkill -P $$; exit" SIGINT
+trap "pkill -P $$; exit" SIGTERM
+trap "pkill -P $$; exit" SIGQUIT
+
+if [ $# -eq 2 ]; then
+	IP=$1
+else
+	IP="127.0.0.1"
+fi
+
+make re -C ../
+make re -C ./resources/terminator/
+rm -rf diff/$1
+
+../ircserv 6667 6667 > /dev/null 2> /dev/null&
+clear
+
 CASE=$(/bin/ls ./testcases/case/$1)
 for case in ${CASE[@]}
 do
 	echo $1
 	echo $case
-	./resources/terminator/tester ./testcases/case/$1/$case $2
+	./resources/terminator/tester ./testcases/case/$1/$case $IP
 	read
 	clear
 done
+
+make fclean -C ../
+make fclean -C ./resources/terminator/
+
+pkill -P $$
--- testall.sh
+++ testall.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-trap "pkill -P $$" SIGINT
-trap "pkill -P $$" SIGTERM
-trap "pkill -P $$" SIGQUIT
+trap "pkill -P $$; exit" SIGINT
+trap "pkill -P $$; exit" SIGTERM
+trap "pkill -P $$; exit" SIGQUIT
+
+if [ $# -eq 0 ]; then
+	IP="127.0.0.1"
+else
+	IP=$1
+fi
 
 make re -C ../
 make re -C ./resources/terminator/
@@ -16,13 +22,18 @@ TEST=$(/bin/ls ./testcases/case)
 for test in ${TEST[@]}
 do
 	clear
-	./test.sh $test $1
+	CASE=$(/bin/ls ./testcases/case/$test)
+	for case in ${CASE[@]}
+	do
+		echo $test
+		echo $case
+		./resources/terminator/tester ./testcases/case/$test/$case $IP
+		read
+		clear
+	done
 done
 
 make fclean -C ../
 make fclean -C ./resources/terminator/
-
-
-
 
 pkill -P $$
